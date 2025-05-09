fe11cdbb57e782ddea7b794a8fc1e85afe906876
cris <christty_92@hotmail.com>
2025-05-09 11:40:24 +0200

    addlist

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 'use strict'
 
 const tasks = [
-    { name: "Recoger setas en el campo", completed: true, id: 1 },
-    { name: "Comprar pilas", completed: true, id: 2 },
-    { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-    {name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-      completed: false,
-      id: 4,
-    },
-  ];
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
 
-  for (const task of tasks) {
-    // pintar la tarea en la lista
-  }
+let taskElement = "";
+let lista = document.querySelector(".task-list");
+let task ="";
+
+for (const task of tasks) {
+  taskElement += `<li>${task.name}</li>`; // Mostramos solo el nombre
+  console.log(task.name); // Mostramos el nombre en la consola
+  
+}
+
+lista.innerHTML = taskElement; // Añadimos el contenido a la lista
+
+    
