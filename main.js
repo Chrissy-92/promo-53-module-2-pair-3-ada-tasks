e9a65b8fcae8439e65985af4a0ada98781a25b49
Cristina Pérez <cristinaperezhuerta@gmal.com>
2025-05-12 11:42:51 +0200

    AdaTasks excersice Métodos funcionales de Array 1

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 'use strict'
+const texTaskAdd = document.querySelector('.js-text-task-add');
+const btnAdd = document.querySelector('.js-btn-add');
 
 const tasks = [
   { name: "Recoger setas en el campo", completed: true, id: 1 },
@@ -6,7 +8,7 @@ const tasks = [
   { name: "Poner una lavadora de blancos", completed: true, id: 3 },
   {
     name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
+    completed: true,
     id: 4,
   },
 ];
@@ -16,11 +18,35 @@ let lista = document.querySelector(".task-list");
 let task ="";
 
 for (const task of tasks) {
-  taskElement += `<li>${task.name}</li>`; // Mostramos solo el nombre
-  console.log(task.name); // Mostramos el nombre en la consola
-  
-}
+  taskElement += `<li>
+      <input 
+        type="checkbox" 
+        id="task-${task.id}" 
+        ${task.completed ? "" : "checked"}
+      />
+      <label for="task-${task.id}">${task.name}</label>
+    </li>`;
+};
 
 lista.innerHTML = taskElement; // Añadimos el contenido a la lista
 
+// checkbox de la lista
+lista.addEventListener("change", (ev) => {
+  // ¿Ha cambiado un checkbox?
+  if (ev.target.matches('input[type="checkbox"]')) {
+    const checkbox = ev.target;
+    // Buscamos el <label> asociado
+    const label = lista.querySelector(`label[for="${checkbox.id}"]`);
+    // Toggle de la clase .tachado según esté marcado o no
+    label.classList.toggle("tachado", checkbox.checked);
+  }
+});
+
+btnAdd.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  const taskValue = texTaskAdd.value;
+  taskElement = taskValue;
+  lista.innerHTML += taskElement;
+  console.log(taskValue);
+});
     
